4ca5a02b2a223eed4b9944dbe78f2e7992e02cbe
MrMarexo <mr.marexo@gmail.com>
2021-01-28 07:30:32 +0100

    Basic Functionality

---
+++ ChallengeAccepted/Assets/ChallengeList.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChallengeList : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] Transform contextTransform; 
+    
+    List<string> textList = new List<string>{ "1", "2", "3", "4", "5", "6", "7", "8" };
+
+
+    void Start()
+    {
+        foreach(string item in textList)
+        {
+            var newText = Instantiate(prefab, contextTransform);
+            newText.GetComponent<TextMeshProUGUI>().text = item;
+        }
+    }
+
+    public void Generate()
+    {
+        int playerCount = GetComponent<PlayerList>().GetPlayerCount();
+        var results = GameObject.FindGameObjectsWithTag("result");
+
+        for (int i = 0; i < playerCount; ++i)
+        {
+            var resIndex = Random.Range(0, textList.Count);
+            results[i].GetComponent<TextMeshProUGUI>().text = textList[resIndex];
+        }
+    }
+
+   
+
+
+}
---
+++ ChallengeAccepted/Assets/PlayerList.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerList : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] Transform listTransform;
+
+    [SerializeField] GameObject plus;
+    [SerializeField] GameObject minus;
+
+
+    int playerCount = 1;
+
+    private void Start()
+    {
+        CheckSigns();
+    }
+
+
+    public void AddPlayer()
+    {
+        int listChildrenCount = listTransform.childCount;
+        var newPlayer = Instantiate(prefab, listTransform);
+        newPlayer.transform.SetSiblingIndex(listChildrenCount - 1);
+        ++playerCount;
+        CheckSigns();
+
+    }
+
+    public void RemovePlayer()
+    {
+        int listChildrenCount = listTransform.childCount;
+        var itemToRemove = listTransform.GetChild(listChildrenCount - 2);
+        Destroy(itemToRemove.gameObject);
+        --playerCount;
+        CheckSigns();
+
+    }
+
+    void CheckSigns()
+    {
+        if (playerCount <= 1) minus.SetActive(false);
+        else minus.SetActive(true);
+
+        if (playerCount >= 4) plus.SetActive(false);
+        else plus.SetActive(true);
+    }
+
+    public int GetPlayerCount()
+    {
+        return playerCount;
+    }
+
+}
